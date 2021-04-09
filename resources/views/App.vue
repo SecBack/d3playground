c5f2a0896f57f4c56233cae65e26c829330dc04e
SecBack <albert3257@live.dk>
2021-04-09 14:47:36 +0200

    some setup done, test webpack stuff

---
+++ resources/views/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  app
+  <v-main>
+    <v-container fluid>
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+</div>
+</template>
+
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+
+  },
+  methodes() {
+
+  }
+}
+</script>
